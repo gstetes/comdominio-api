3b90ca07613b1cc2f17536062e8c0cf9b32c0a97
Guilherme Stetes <guilherme.stetes@tagchat.com.br>
2022-11-16 12:01:20 -0300

    finish users controller

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,15 +1,140 @@
 const connection = require('../database/connection');
+const bcrypt = require('bcrypt');
 
 const create = async (req, res) => {
-  connection('users').insert({
-    name: 'Guilherme',
-    email: 'gstetes@gmail.com',
-    password: '123'
-  })
-    .then(user => res.json({ user }))
-    .catch(error => res.json({ error }))
+  const { name, email, password } = req.body;
+
+  const userAlreadyExists =
+    await connection('users')
+      .select('*')
+      .where('email', email)
+      .first();
+
+  if (!!userAlreadyExists) {
+    return res.status(400)
+      .json({
+        error: 'User with this email already exists',
+      });
+  };
+
+  const user = {
+    name,
+    email,
+    password: await bcrypt.hash(password, 16),
+  };
+
+  try {
+    await connection('users')
+      .insert(user);
+  } catch (error) {
+    return res.status(500).json({ error: err });
+  };
+
+  return res.status(200).json();
+};
+
+const list = async (req, res) => {
+  const users = await connection(`users`).select(`*`);
+
+  return res.status(200).json(users);
+};
+
+const search = async (req, res) => {
+  const { id } = req.params;
+
+  const user = await connection('users')
+    .select('*')
+    .where('id', id)
+    .first();
+
+  if (!!user) {
+    delete user.password;
+
+    return res.status(200)
+      .json(user);
+  } else {
+    return res.status(404)
+      .json({
+        error: 'User not found',
+      });
+  };
+};
+
+const update = async (req, res) => {
+  const { id } = req.params;
+
+  const userExists =
+    await connection('users')
+      .select('*')
+      .where('id', id)
+      .first();
+
+  if (!userExists) {
+    return res.status(404)
+      .json({
+        error: 'User does not exists.'
+      });
+  };
+
+  if (!!req.password) {
+    req.password = await bcrypt.hash(req.password, 16);
+  };
+
+  const newUserData = {
+    id,
+    ...req.body,
+  };
+
+  try {
+    await connection('users')
+      .select('*')
+      .where('id', id)
+      .first()
+      .update(newUserData);
+
+    return res.status(200)
+      .json();
+
+  } catch (error) {
+    return res.status(500)
+      .json({ error })
+  }
+};
+
+const destroy = async (req, res) => {
+  const { id } = req.params;
+
+  const userExists = await connection('users')
+    .select('*')
+    .where('id', id)
+    .first();
+
+  if (!userExists) {
+    return res.status(404)
+      .json({
+        error: 'User does not exists.'
+      });
+  };
+
+  try {
+    await connection('users')
+      .select('*')
+      .where('id', id)
+      .first()
+      .delete();
+
+    return res.status(200)
+      .json();
+  } catch (error) {
+    return res.status(500)
+      .json({ error })
+  }
 };
 
 module.exports = {
   create,
+  list,
+  search,
+  update,
+  destroy,
 }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,6 +2,10 @@ const routes = require('express')();
 
 const usersControllers = require('../controllers/UsersController');
 
-routes.post('/create', usersControllers.create);
+routes.post('/', usersControllers.create);
+routes.get('/', usersControllers.list);
+routes.get('/:id', usersControllers.search);
+routes.put('/:id', usersControllers.update);
+routes.delete('/:id', usersControllers.destroy);
 
 module.exports = routes;
